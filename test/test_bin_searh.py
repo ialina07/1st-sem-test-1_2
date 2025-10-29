87039503cb80d788b20458c8496ebb5763efe1b8
Efim Kubyshkin <efimkub@mail.ru>
2025-10-29 20:50:17 +0300

    Add bin_search with tests

---
+++ test/test_bin_searh.py
@@ -0,0 +1,13 @@
+from src.bin_search import binSearch
+
+
+def test_middle():
+    assert binSearch([1, 2, 3, 4, 5], 4) == 3
+
+
+def test_start():
+    assert binSearch([1, 2, 3, 4], 2) == 1
+
+
+def test_not_in_list():
+    assert binSearch([1, 2, 3, 4], 5) == -1
