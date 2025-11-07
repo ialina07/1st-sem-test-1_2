e51b0e4680126d589a7e932be42972b2edbadced
ialina07 <alinaiva07@mail.ru>
2025-11-07 14:28:46 +0000

    добавлен дополненный тест для checksum.py

--- test/test_checksum.py
+++ test/test_checksum.py
@@ -1,9 +1,32 @@
-from src.checksum import modulo11Checksum
+from src.checksum import modulo11_checksum
 
 
 def test_good():
-    assert modulo11Checksum("2-266-11156-8")
+    assert modulo11_checksum("2-266-11156-8")
 
 
 def test_bad():
-    assert not modulo11Checksum("2-266-11156-3")
+    assert not modulo11_checksum("2-266-11156-3")
+
+def test_good_simple():
+    assert modulo11_checksum("0306406152")
+
+
+def test_bad_simple():
+    assert not modulo11_checksum("0306406153")
+
+
+def test_short():
+    assert not modulo11_checksum("123")
+
+
+def test_empty():
+    assert not modulo11_checksum("")
+
+
+def test_with_spaces():
+    assert modulo11_checksum("0 306 40615 2")
+
+
+def test_with_x():
+    assert modulo11_checksum("012000030X")
